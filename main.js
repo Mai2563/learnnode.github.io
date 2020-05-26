e5c1038bdd5912a61de9be9a7af53408d90aa2fe
Mai <nawaphat2563@gmail.com>
2020-05-26 16:48:39 +0700

    Create File

--- main.js
+++ main.js
@@ -6,10 +6,26 @@ var fs = require('fs')
 
 //Create server
 http.createServer(function (request, response){
+    //Read file
     fs.readFile('demofile1.html', function(err, data){
         response.writeHead(200, {'Content-Type': 'text/html'})
         response.write(data)
         return response.end()
+    })
+    //Create file
+    fs.appendFile('mynewfile1.txt', 'Hello Content', function (err){
+        if (err) throw err;
+        console.log('Saved!')
+    })
+
+    fs.open('mynewfile2.txt', 'w', function (err, file){
+        if (err) throw err
+        console.log('Saved!')
+    })
+
+    fs.writeFile('mynewfile3.txt', 'Hello', function (err){
+        if (err) throw err
+        console.log('Saved!')
     })
 }).listen(8081)
 
