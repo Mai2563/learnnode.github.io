351329d17d0a77c426b2a790feebf36f0d9fbfe7
Mai <nawaphat2563@gmail.com>
2020-05-26 15:00:21 +0700

    Split the query string

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 //Import require module
 var http = require('http')
-var dt = require('./myfirstmodule')
+// var dt = require('./myfirstmodule')
+var url = require('url')
+
 //Create server
 http.createServer(function (request, response){
     //Send the HTTP header
     response.writeHead(200, {'Content-Type':'text/html'})
-    // response.writeHead(200, {'Content-Type': 'text/plain'})
+        // response.writeHead(200, {'Content-Type': 'text/plain'})
     //Send the response body
-    response.write("The date and time are currently: " + dt.myDateTime())
-    // response.end("hello")
-    response.end()
+        // response.write("The date and time are currently: " + dt.myDateTime())
+        // response.end("hello")
+    //Get url ex. /summer
+        // response.write(request.url)
+    //Split the Query String ex. ?year=2017&month=July
+    var q = url.parse(request.url, true).query
+    var txt = q.year + " " + q.month
+    response.end(txt)
 }).listen(8081)
 
 //console will print the message
