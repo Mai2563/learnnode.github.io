9d3414d4649dd3d2339bcf1002b5cb3efd0bf99c
Mai <nawaphat2563@gmail.com>
2020-05-26 14:40:57 +0700

    Include own module

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 //Import require module
 var http = require('http')
+var dt = require('./myfirstmodule')
 //Create server
 http.createServer(function (request, response){
     //Send the HTTP header
     response.writeHead(200, {'Content-Type':'text/html'})
     // response.writeHead(200, {'Content-Type': 'text/plain'})
     //Send the response body
-    response.end("hello")
+    response.write("The date and time are currently: " + dt.myDateTime())
+    // response.end("hello")
+    response.end()
 }).listen(8081)
 
 //console will print the message
-console.log('Server running')
+console.log('Server running at http://localhost:8081')
