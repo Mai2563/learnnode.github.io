1049a86f32e2fc1e2b37a3ab294cab51e7be232f
Mai <nawaphat2563@gmail.com>
2020-05-26 15:21:38 +0700

    Read Files

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
 //Import require module
 var http = require('http')
 // var dt = require('./myfirstmodule')
-var url = require('url')
+// var url = require('url')
+var fs = require('fs')
 
 //Create server
 http.createServer(function (request, response){
-    //Send the HTTP header
-    response.writeHead(200, {'Content-Type':'text/html'})
-        // response.writeHead(200, {'Content-Type': 'text/plain'})
-    //Send the response body
-        // response.write("The date and time are currently: " + dt.myDateTime())
-        // response.end("hello")
-    //Get url ex. /summer
-        // response.write(request.url)
-    //Split the Query String ex. ?year=2017&month=July
-    var q = url.parse(request.url, true).query
-    var txt = q.year + " " + q.month
-    response.end(txt)
+    fs.readFile('demofile1.html', function(err, data){
+        response.writeHead(200, {'Content-Type': 'text/html'})
+        response.write(data)
+        return response.end()
+    })
 }).listen(8081)
 
 //console will print the message
